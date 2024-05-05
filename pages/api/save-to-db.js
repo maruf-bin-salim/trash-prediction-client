efc5f815cb74c445536868dd0fd9071b0e403d33
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-06 02:53:26 +0600

    removed the demo data for saving to db

--- pages/api/save-to-db.js
+++ pages/api/save-to-db.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     res.status(405).json({ message: "Method not allowed" });
     return;
   }
-  const data = { trash: 'trash' } || req.body;
+  const data = req.body;
   try {
     await addTrashData(data);
     res.status(200).json({ message: "Data added successfully" });
