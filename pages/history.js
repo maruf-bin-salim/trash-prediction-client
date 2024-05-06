4856da4b14836248c9e42320014d54fe76a69d13
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-06 14:21:43 +0600

    u

--- pages/history.js
+++ pages/history.js
@@ -8,11 +8,32 @@ import { useEffect, useState } from "react"
 export default function History() {
 
     const [trashData, setTrashData] = useState([])
+    const [classes, setClasses] = useState(["cardboard", "glass", "metal", "paper", "plastic", "trash"]);
+    const [countOfClasses, setCountOfClasses] = useState({});
+
+    async function handleRemoveAllPressed() {
+        const trashCollection = collection(database, "trash");
+        const snapshot = await getDocs(trashCollection);
+        snapshot.forEach(async (doc) => {
+            await deleteDoc(doc.ref);
+        });
+    }
+
+
 
     useEffect(() => {
         const trashCollection = collection(database, "trash");
         const unsubscribe = onSnapshot(trashCollection, (snapshot) => {
             const trashList = snapshot.docs.map(doc => doc.data());
+            // set count of classes
+            let count = {};
+            classes.forEach((c) => {
+                count[c] = 0;
+            });
+            trashList.forEach((trash) => {
+                count[trash.prediction.name] += 1;
+            });
+            setCountOfClasses(count);
             setTrashData(trashList);
         });
 
@@ -26,6 +47,24 @@ export default function History() {
     return (
         <div>
             <h1>History</h1>
+            <button onClick={handleRemoveAllPressed}>
+                Remove All
+            </button>
+
+            <h2>
+                Total Trash Predicted: {trashData.length}
+            </h2>
+            <h2>Count of Classes</h2>
+            {
+                classes.map((data, index) => {
+                    return (
+                        <div key={index}>
+                            <p>{data}: {countOfClasses[data]}</p>
+                        </div>
+                    )
+
+                })
+            }
             {
                 trashData?.map((data, index) => {
                     return (
